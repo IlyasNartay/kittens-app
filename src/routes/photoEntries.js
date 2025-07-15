77a112e1d82fd911a574ad6b4caf8d96cfbffc7e
Ilyas Nartay <ilyas.nartay@gmail.com>
2025-07-16 03:07:41 +0500

    update

--- src/routes/photoEntries.js
+++ src/routes/photoEntries.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/PhotoEntry');
+const controller = require('../controllers/photoEntry');
 const multer = require('multer');
 const { storage } = require('../utils/cloudinary');
 const upload = multer({ storage });
